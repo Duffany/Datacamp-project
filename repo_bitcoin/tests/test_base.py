7c7fab8784c3fc85a80214e4db0cc81a058df6ed
Doufani <68777600+Duffany@users.noreply.github.com>
2020-12-17 17:17:11 +0100

    black formatting

--- repo_bitcoin/tests/test_base.py
+++ repo_bitcoin/tests/test_base.py
@@ -4,3 +4,6 @@ from repo_bitcoin.base import add_int
 def test_add_int():
     res = add_int(1, 2)
     assert res == 3
+    
+df = pd.DataFrame(list(zip(I1,I2,I3,I4,I5,I6,I7,I8,I9,I10,X)), columns = ['I1', 'I2', 'I3', 
+                                                             'I4', 'I5', 'I6', 'I7', 'I8', 'I9', 'I10', 'X'])
